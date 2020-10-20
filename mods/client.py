376885fd9704440653894ca64f8c83f8649e2fbb
krishpranav <krisna.pranav@gmail.com>
2020-10-20 19:28:57 +0530

    done changes

--- mods/client.py
+++ mods/client.py
@@ -8,3 +8,9 @@ class CLIENT:
     def __init__(self, _ip, _pt):
         self.ipaddress = _ip
         self.port      = _pt
+    
+    def send_data(self, tosend, encode=True):
+        if encode:
+            self.SOCK.send(base64.encodebytes(tosend.encode('utf-8')) + self.KEY.encode('utf-8'))
+        else:
+            self.SOCK.send(base64.encodebytes(tosend) + self.KEY.encode('utf-8'))
