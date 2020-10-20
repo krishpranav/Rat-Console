796dd220307d279321e6c159d5604a13a61ff29e
krishpranav <krisna.pranav@gmail.com>
2020-10-20 19:27:57 +0530

    done changes

---
+++ mods/client.py
@@ -0,0 +1,10 @@
+class CLIENT:
+
+    SOCK = None
+    KEY  = ")J@NcRfU"
+    KEYLOGGER_STATUS = False
+    KEYLOGGER_STROKES = ""
+
+    def __init__(self, _ip, _pt):
+        self.ipaddress = _ip
+        self.port      = _pt
