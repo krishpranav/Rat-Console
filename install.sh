7660fb20f2a6d621e0e638eb4d2dc12721b57c3b
krishpranav <krisna.pranav@gmail.com>
2021-01-10 11:17:57 +0530

    done changes

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ echo "Installing Dependencies and python modules"
 python3 -m pip install -r requirements.txt
 sudo chmod +x console.py
 clear
-echo "Now you can run python3 console.py"
+echo "Now You Can Run python3 console.py"
