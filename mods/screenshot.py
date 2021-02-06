9969ce27b189305000666454474bcbc358d2c904
krishpranav <krisna.pranav@gmail.com>
2021-02-06 15:34:44 +0530

    done changes

--- mods/screenshot.py
+++ mods/screenshot.py
@@ -1,4 +1,16 @@
+#!/usr/bin/env/python
+
 class SCREENSHOT:
 
     SC_DATA = b""
 
+    
+    def __init__(self):
+        self.generate()
+    
+
+    def generate(self):
+        obj = io.BytesIO()
+        im = pyscreenshot.grab()
+        im.save(obj, format="PNG")
+        self.SC_DATA = obj.getvalue()
