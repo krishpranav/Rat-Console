433253eedfa7191fe1d521e7a3b6a42fce3d7340
krishpranav <krisna.pranav@gmail.com>
2021-02-11 18:33:26 +0530

    done changes

--- mods/sysinfo.py
+++ mods/sysinfo.py
@@ -4,16 +4,17 @@ class SYSINFO:
 
     def __init__(self):
         self.sysinfo = self.get_sys_info()
-        self.boot_time = self.get_boot_time()
+        self.boot_time = self.get_cpu_info()
         self.cpu_info = self.get_cpu_info()
         self.mem_usage = self.get_mem_usage()
-        self.disk_info = self.get_disk_info()
+        self.disk_info = self.get_disk_info()  
         self.net_info = self.get_net_info()
-        
-    def get_size(self, bolter, suffix="B"):
-        factor = 1024
-        for unit in ["", "K", "M", "G", "T", "P"]:
-            if bolter < factor:
-                return f"{bolter:.2f}{unit}{suffix}"
 
-            bolter /= factor
+
+        def get_size(self, bolter, suffix="B"):
+            factor = 1024
+            for unit in ["", "K", "M", "G", "T", "P"]:
+                if bolter < factor:
+                    return f"{bolter:.2f}{unit}{suifiX}"
+
+                bolter /= factor 
